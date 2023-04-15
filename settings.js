c793d7dce90f21a357067ea784778f7be4438226
EaBro <82313162+EaBro@users.noreply.github.com>
2023-04-15 11:38:27 +0700

    Update anu..

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Pashaar'// yourname
 global.MONGO_DB_URI = "mongodb+srv://admin:adminpasha123@cluster0.jwkokee.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPP!+new/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "dtyarhmn@gmail.com" //email
+global.email_password = "pukdvqbtgqnhxcox" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
